1339dc7abbfd65a8989055b281ce90d57cd17928
BouYingWong <kid87927605@gmail.com>
2019-01-06 17:20:47 +0800

    first commit

---
+++ src/main/java/com/sheldon/real/time/communication/RealTimeCommunicationApplication.java
@@ -0,0 +1,14 @@
+package com.sheldon.real.time.communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeCommunicationApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/sheldon/real/time/communication/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.sheldon.real.time.communication.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String testController(){
+        return "123456";
+    }
+}
