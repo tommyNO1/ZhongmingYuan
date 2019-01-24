1a3e3607c1a0b38094c44ca3f7167c7c685cad06
BouYingWong <kid87927605@gmail.com>
2019-01-24 23:36:09 +0800

    解决数据库连接问题，丢那吗

--- src/main/java/com/sheldon/real/time/communication/RealTimeCommunicationApplication.java
+++ src/main/java/com/sheldon/real/time/communication/RealTimeCommunicationApplication.java
@@ -1,9 +1,13 @@
 package com.sheldon.real.time.communication;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.sheldon.real.time.communicate.mapper")
 public class RealTimeCommunicationApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/sheldon/real/time/communication/RealTimeCommunicationApplicationTests.java
+++ src/test/java/com/sheldon/real/time/communication/RealTimeCommunicationApplicationTests.java
@@ -1,16 +1,31 @@
 package com.sheldon.real.time.communication;
 
+
+import com.sheldon.real.time.communication.entity.User;
+import com.sheldon.real.time.communication.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = RealTimeCommunicationApplication.class)
 public class RealTimeCommunicationApplicationTests {
+
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     public void contextLoads() {
+        List<User> users = userMapper.selectAll();
+        for (User user:users) {
+            System.out.println(user.getUsername());
+        }
     }
 
 }
